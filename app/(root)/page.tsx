3c93f518aee2fdfc210ea5f3d22baa8ad3fd0a59
malak-cs28 <m.mohamed2386@nu.edu.eg>
2025-12-03 16:41:08 +0200

    implement headerbox component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,24 @@
+import HeaderBox from "@/components/ui/HeaderBox";
 import react from "react";
 
 const Home = () => {
+  const loggedIn = {firstName: 'Malak'};
   return (
-    <div>Home</div>)
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+          type="greeting"
+          title="Welcome to NU Bank"
+          user={loggedIn?.firstName || "Customer"}
+          subtext="Your modern banking platform for everyone."
+          />
+
+        </header>
+      </div>
+
+    </section>
+  );
 };
 
 export default Home;
