ecab1dd2c1d128b73e710a6e325d1e05c1ff63f9
Sara Mohamed <s.mohamed2317@nu.edu.eg>
2025-12-11 01:13:05 +0200

    work in progress, i am starting the code again as it is not working

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,25 +1,18 @@
-import MobileNav from "@/Banking-system2/components/MobileNav";
 
+//import MobileNav from "@/Banking-system2/components/MobileNav";
+ //import Sidebar from "@/Banking-system2/components/Sidebar"
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const loggedIn = { firstName: "Adrian", lastName: "JSM" }as User;
+  //const loggedIn = { firstName: "Adrian", lastName: "JSM" }as User;
 
   return (
     <main className="flex h-screen w-full font-inter">
-      <div className="flex h-full w-full flex-col">
-        <div className="root-layout flex items-center gap-3 px-4 py-2">
-          <img src="/icons/logo.svg" width={30} alt="logo" />
-
-          <div>
-            <MobileNav user={loggedIn} />
-          </div>
-        </div>
+      Sidebar
 
         {children}
-      </div>
     </main>
   );
 }
