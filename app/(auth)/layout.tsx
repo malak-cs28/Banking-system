af6b6c4318cb292d32b518577b9311dd3ffa7127
malak-cs28 <m.mohamed2386@nu.edu.eg>
2025-12-12 21:36:46 +0200

    Setup: initial Sentry integration changes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -5,6 +6,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main>{children}</main>
+    <main className="flex min-h-screen w-full justify-between font-inter">{children}
+    <div className="auth-asset">
+      <div>
+        <Image
+          src="/icons/auth-image.svg"
+          alt="Auth image"
+          width={400}
+          height={500}
+        />
+
+      </div>
+      </div></main>
   );
 }
