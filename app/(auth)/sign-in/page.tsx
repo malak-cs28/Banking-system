812f79523fbbee9d150a3ff9e8c7beeb2a96084a
Sara Mohamed <s.mohamed2317@nu.edu.eg>
2025-12-13 20:49:16 +0200

    Working version of sidebars the right sidebar and mobile nav (final )

--- app/(auth)/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-import react from "react";
-
-const SignIn = () => {
-  return (
-    <div>SignIn</div>
-)
-};
-
-export default SignIn;
--- app/(auth)/sign-up/page.tsx
+++
@@ -1,9 +0,0 @@
-import react from "react";
-
-const SignUp = () => {
-  return (
-    <div>SignUp</div>
-)
-};
-
-export default SignUp;
