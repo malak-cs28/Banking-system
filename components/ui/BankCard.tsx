76ef265d2c497c0033cc968818b449e094f32593
malak-cs28 <m.mohamed2386@nu.edu.eg>
2025-12-17 12:41:19 +0200

    Revert "Merge pull request #24 from BankingSystemOrg/displayingRealdata"
    This reverts commit 76e5a7f39699a28ff50bb6ca4edf27013d3a7f6e, reversing
    changes made to 54dbad317037477659c7ca7518cfb6583bba5f67.

--- components/ui/BankCard.tsx
+++ components/ui/BankCard.tsx
@@ -2,7 +2,6 @@ import { formatAmount } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import Copy from './Copy'
  
 
 const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) => {
@@ -61,7 +60,6 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
         />
       </Link>
 
-      {showBalance && <Copy title={account?.sharaebleId} />}
       
     </div>
   )
